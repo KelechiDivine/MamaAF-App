c24398e866dc86e9dc8383a29bb35bbfc028eb27
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-03 17:08:24 +0100

    setup project for programming blog application

---
+++ src/main/java/com/pt/programmingblogapp/ProgrammingBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.pt.programmingblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingBlogAppApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingBlogAppApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/pt/programmingblogapp/ProgrammingBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.programmingblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingBlogAppApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
